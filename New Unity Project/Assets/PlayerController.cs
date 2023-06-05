75e2b3d132b31c7b38f729a06a9633d3575a7a0e
Horstlp <christian.jeitler07@gmail.com>
2023-06-05 14:29:59 +0200

    movement

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 10;
+    public int sprintSpeed = 20;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        rb.velocity = movement * speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = 10;
+        }
+    }
+}
